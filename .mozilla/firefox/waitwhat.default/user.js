44f51dff88dd23735192f557feeb90473a9b22a9
Asbjørn Apeland <aude@users.noreply.github.com>
2016-01-02 21:50:08 +0100

    homepage

--- .mozilla/firefox/waitwhat.default/user.js
+++ .mozilla/firefox/waitwhat.default/user.js
@@ -1,2 +1,4 @@
 user_pref("browser.search.defaultenginename", "DuckDuckGo (SSL)");
 user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.startup.page", 0);
+user_pref("browser.startup.homepage", "https://duckduckgo.com/");
