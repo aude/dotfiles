829a48cea5bcbeb89e7e406b3ca4a2e600ff6ade
Asbjørn Apeland <aude@users.noreply.github.com>
2016-02-19 14:39:22 +0100

    gpg-agent: improved initialization

--- .profile.d/80aude.sh
+++ .profile.d/80aude.sh
@@ -1,9 +1,9 @@
 # -- env --
 path_dirs=( ~/.local/bin ~/bin )
 for dir in ${path_dirs[@]}; do
-    if [[ -z $(grep "$dir" <<< $PATH) ]]; then
-        export PATH=$dir:$PATH
-    fi
+	if [[ -z $(grep "$dir" <<< $PATH) ]]; then
+		export PATH=$dir:$PATH
+	fi
 done
 
 export EDITOR=vim
@@ -11,25 +11,38 @@ export PASSWORD_STORE_DIR=~/key/password-store/
 export GOPATH=~/dev/go
 
 # gpg-agent
-if ! pgrep -u "$(whoami)" 'gpg-agent' >/dev/null 2>&1; then
-	eval "$(gpg-agent --daemon)"
-
-	# set $SSH_AGENT to something that will not run,
-	# so RedHat's xinitrc-common does not launch a new ssh-agent
-	export SSH_AGENT=' '
+# use gpg-agent's SSH agent emulation
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+	export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh"
 fi
+
+# set $SSH_AGENT to something that will not run,
+# so RedHat's xinitrc-common does not launch a new ssh-agent
+export SSH_AGENT=' '
 
 # virtualenvwrapper
 #VEW=/usr/share/virtualenvwrapper/virtualenvwrapper.sh
 VEW=/usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
 if [[ -f $VEW ]]; then
-    export WORKON_HOME=~/dev/py/env
+	export WORKON_HOME=~/dev/py/env
 fi
 
 # -- source --
 #source_these=()
 #for file in "${source_these[@]}"; do
-#    if [[ -f $file ]]; then
-#        source "$file"
-#    fi
+#	if [[ -f $file ]]; then
+#		source "$file"
+#	fi
 #done
+
+# -- cmd --
+# do not show the "Done" message, so run all in subshell
+(
+	(
+		# gpg-agent
+		if ! pgrep -u "$(whoami)" 'gpg-agent' >/dev/null 2>&1; then
+			gpg-connect-agent /bye >/dev/null 2>&1
+		fi
+	)&
+)
