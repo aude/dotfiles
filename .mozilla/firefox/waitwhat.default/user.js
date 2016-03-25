6167fe00673dab6483431e86be2f67b890898e67
Asbjørn Apeland <aude@users.noreply.github.com>
2016-03-25 22:28:12 +0100

    xedit

--- .mozilla/firefox/waitwhat.default/user.js
+++ .mozilla/firefox/waitwhat.default/user.js
@@ -2,3 +2,4 @@ user_pref('browser.search.defaultenginename', 'ddg');
 user_pref('browser.newtabpage.enabled', false);
 user_pref('browser.startup.page', 0);
 user_pref('browser.startup.homepage', 'https://duckduckgo.com/');
+user_pref('extensions.greasemonkey.editor', '~/bin/xedit');
