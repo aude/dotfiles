a12104b6e68fda881a7dc32f3ad551861e42e6cc
Asbjørn Apeland <aude@users.noreply.github.com>
2016-07-29 19:24:52 +0200

    profile: update SSH agent socket path

--- .profile.d/80aude.sh
+++ .profile.d/80aude.sh
@@ -9,7 +9,7 @@ export ANDROID_HOME=~/src/android-sdk-linux
 # use gpg-agent's SSH agent emulation
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-	export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh"
+	export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
 fi
 
 # set $SSH_AGENT to something that will not run,
