27a432b097127853f8f51af3ec19d503a78467fb
Asbjørn Apeland <aude@users.noreply.github.com>
2018-11-01 17:40:34 +0100

    .profile: spaces > tabs

--- .profile.d/80aude.sh
+++ .profile.d/80aude.sh
@@ -10,7 +10,7 @@ export MPV_SOCKET="$XDG_RUNTIME_DIR/mpv-socket"
 # use gpg-agent's SSH agent emulation
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-	export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
+    export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
 fi
 
 # set $SSH_AGENT to something that will not run,
@@ -42,17 +42,17 @@ export MAVEN_OPTS='-XX:+TieredCompilation -XX:TieredStopAtLevel=1'
 
 path_dirs=( ~/.local/bin ~/bin $GOPATH/bin $N_PREFIX/bin $ANDROID_HOME/tools )
 for dir in "${path_dirs[@]}"; do
-	if [[ $PATH != *$dir* ]]; then
-		export PATH=$dir:$PATH
-	fi
+    if [[ $PATH != *$dir* ]]; then
+        export PATH=$dir:$PATH
+    fi
 done
 unset path_dirs dir
 
 ## -- source --
 #source_these=()
 #for file in "${source_these[@]}"; do
-#	if [[ -f $file ]]; then
-#		source "$file"
-#	fi
+#    if [[ -f $file ]]; then
+#        source "$file"
+#    fi
 #done
 #unset source_these
